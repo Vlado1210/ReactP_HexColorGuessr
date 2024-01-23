9a3cf795a49f463cacc51689671f984a9444360d
Vlado1210 <vladochavez19@gmail.com>
2024-01-22 19:44:26 -0600

    project upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [ randomColor , setRandomColor] = useState([]);
+  const [ answerPosition, setAnswerPosition ] = useState(Math.floor(Math.random() * 3))
+  const [ result , setResult] = useState("")
+
+  const generateRandomColors = (idx) =>{
+    if ( answerPosition === idx){
+      setResult("Correct")
+    }
+    else{
+      setResult("Incorrect")
+    }
+    const newColors = [Math.random().toString(16).substring(9),Math.random().toString(16).substring(9),Math.random().toString(16).substring(9)];
+    setRandomColor(newColors);
+    setAnswerPosition(Math.floor(Math.random()*3))
+  }
+
+  useEffect(()=>{
+    const newColors = [Math.random().toString(16).substring(9),Math.random().toString(16).substring(9),Math.random().toString(16).substring(9)];
+    setRandomColor(newColors);
+    setAnswerPosition(Math.floor(Math.random()*3))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="colorSqr" style={{background: `#${randomColor[answerPosition]}`}}>
+      </div>
+      <div>
+        {randomColor.map((button , idx)=>{
+          return(<button key={idx} onClick={() => generateRandomColors(idx)}>{button}</button>)
+        })}
+      </div>
+      <div>
+        <h2>{result}</h2>
+      </div>
     </div>
   );
 }
